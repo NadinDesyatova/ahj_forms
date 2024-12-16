7942df9371cbdb7eb6c701304d2fe3cf1197a4a2
NadinDesyatova <nadindinina@yandex.ru>
2024-12-16 17:14:14 +0300

    popover.js has corrected

--- src/js/popover.js
+++ src/js/popover.js
@@ -5,7 +5,7 @@ export class Popover {
 
   static get markup() {
     return `
-      <div class="arrow""></div>
+      <div class="popover-arrow""></div>
       <h3 class="popover-header"></h3>
       <div class="popover-body"></div>
     `;
